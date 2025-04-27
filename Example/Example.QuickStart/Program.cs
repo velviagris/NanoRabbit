6376c4b7075890b671999a90b221da4c4fb4b594
Gris <cgc.elvin@outlook.com>
2025-04-27 21:32:47 +0800

    Update Example.QuickStart

--- Example/Example.QuickStart/Program.cs
+++ Example/Example.QuickStart/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using NanoRabbit;
+using NanoRabbit.Service;
 
 var loggerFactory = LoggerFactory.Create(builder =>
 {
@@ -8,26 +9,34 @@ var loggerFactory = LoggerFactory.Create(builder =>
 
 var logger = loggerFactory.CreateLogger("RabbitHelper");
 
-var rabbitHelper = new RabbitHelper(rabbitConfig: new RabbitConfiguration
+var rabbitConfig = new RabbitConfiguration
 {
     HostName = "localhost",
     Port = 5672,
     VirtualHost = "/",
     UserName = "admin",
     Password = "admin",
-    Producers = new List<ProducerOptions> { new ProducerOptions {
+    Producers = new List<ProducerOptions>
+    {
+        new ProducerOptions
+        {
             ProducerName = "FooProducer",
             ExchangeName = "amq.topic",
             RoutingKey = "foo.key",
             Type = ExchangeType.Topic
         }
     },
-    Consumers = new List<ConsumerOptions> { new ConsumerOptions {
-            ConsumerName= "FooConsumer",
+    Consumers = new List<ConsumerOptions>
+    {
+        new ConsumerOptions
+        {
+            ConsumerName = "FooConsumer",
             QueueName = "foo-queue"
         }
     }
-}, logger);
+};
+
+var rabbitHelper = new RabbitHelper(rabbitConfig, logger);
 
 rabbitHelper.Publish<string>("FooProducer", "Hello from NanoRabbit");
 
